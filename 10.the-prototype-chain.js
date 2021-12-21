f6d6f1fc27ba3a3e32838b3175de75a822b846e5
Phil <happy.melodist@gmail.com>
2021-12-20 19:40:05 -0600

    review the prototype chain as they connect to objects and functions

--- 10.the-prototype-chain.js
+++ 10.the-prototype-chain.js
@@ -13,3 +13,5 @@ js.ask("is a great teacher");
 django.ask("is also a great teacher");
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object
+
+// do more of what makes you happy, Phil
